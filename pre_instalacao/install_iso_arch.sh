ce4cf13427aa754148510f3e26d045aa52695e6b
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2019-11-19 19:44:32 -0400

    Primeiro teste de execuçao

---
+++ pre_instalacao/install_iso_arch.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+#
+# install_iso.sh - Download e BOOT da ISO mais recente do Arch Linux
+#
+# Site  : https://github.com/guilhermercarvalho/archinstall
+# Autor : Guilherme Carvalho <guilhermercarvalho512@gmail.com>
+#
+# -------------------------------------------------------------------------------------
+#
+# Realiza Download e BOOT da ISO mais recente do Arch em um pendrive
+#
+# -------------------------------------------------------------------------------------
+#
+# Histórico:
+#
+#   v1.0 2019-10-21, Guilherme Carvalho:
+#       - Versão inicial do programa
+#
+#
+# Licença: -
+#
+#
+#######################################################################################
+#
+#                                     Menssagem
+#                                     ---------
+#
+echo
+echo '-------------------------------------------------------------------------------'
+echo '    ____           __        ____   _________ ____     ___              __  '
+echo '   /  _/___  _____/ /_____ _/ / /  /  _/ ___// __ \   /   |  __________/ /_ '
+echo '   / // __ \/ ___/ __/ __ `/ / /   / / \__ \/ / / /  / /| | / ___/ ___/ __ \'
+echo ' _/ // / / (__  ) /_/ /_/ / / /  _/ / ___/ / /_/ /  / ___ |/ /  / /__/ / / /'
+echo '/___/_/ /_/____/\__/\__,_/_/_/  /___//____/\____/  /_/  |_/_/   \___/_/ /_/ '
+echo '                 Por Guilherme Carvalho - https://github.com/guilhermercarvalho'
+echo '-------------------------------------------------------------------------------'
+echo
+# 
+# 
+#######################################################################################
+#
+#                                        Menu
+#                                        ----
+#   
+echo "Qual das etapas deseja realizar?"
+echo -e "1)Baixar ISO\n2)Bootar ISO em um pendrive\n3)Iniciar Boot pelo pendrive"
+
+read -p "Etapa(s): " opcao
+
+for i in $opcao
+do
+    case $i in
+        1)  echo "### Etapa 1 ###"
+            echo "A ISO mais recente está sendo baixada. Aguarde!"            
+            # Encontra ISO mais recente no repositório mantido pela UFPR
+            l=$(curl -s http://archlinux.c3sl.ufpr.br/iso/latest/ |
+            grep -Eo '\"archlinux-20..\..([1-9]|[10-12])\..([1-9]|[10-31])-x86_64.iso\"' |
+            cut -d\" -f2)               
+            wget -c -O archlinux-x86_64.iso http://archlinux.c3sl.ufpr.br/iso/latest/"$l"
+            ;;
+        2)  echo "### Etapa 2 ###"
+            echo "Este processo irá apagar por completo seu dispositivo."
+            echo "Certifique-se de que o dispositivo CORRETO está selecionado!"
+            sudo fdisk -l
+            echo
+            echo "Selecione o dispositivo /dev/sdx. Apenas a letra."
+            read dispositivo
+            if [[ $dispositivo =~ [a-z] ]]; then
+                echo "Dispositivo selecionado: /dev/sd"$dispositivo""
+                echo
+                read -p "Dispositivo correto?[S/n] " resposta
+                if [[ $resposta =~ ^[Nn]([aA][oO])*$ ]]; then
+                    echo "Programa Finalizado"
+                    exit 1
+                fi
+            else
+                echo "Dispositivo Inválido"
+                exit 1
+            fi
+            echo "Iniciando processo de boot pelo pendrive"
+            echo
+            dd if=archlinux-x86_64.iso of=/dev/sd"$dispositivo" status="progress"
+            echo
+            echo "Processo de boot finalizado!"
+            echo "Reinicie sua máquina dando boot pelo pendrive."
+            exit 0
+            ;;
+        3)  echo "### Etapa 3 ###"
+            echo "Após realizado o download e o BOOT da ISO,"
+            echo "inicialize sua máquina com o pendrive como dispositivo primário de BOOT."
+            echo "Reinicie a máquina"
+            exit 0
+            ;;
+        *)  echo "### Etapa inválida ###"
+        ;;
+    esac            
+done
---
+++ pre_instalacao/pre_install_arch.sh
@@ -0,0 +1,165 @@
+
+#!/usr/bin/env bash
+#
+# pre_install_arch.sh - Pre-Instalação do Arch Linux
+#
+# Site  : https://github.com/guilhermercarvalho/archinstall
+# Autor : Guilherme Carvalho <guilhermercarvalho512@gmail.com>
+#
+# -------------------------------------------------------------------------------------
+#
+# Este programa executa a instalção do Arch Linux em seu sistema
+#
+# -------------------------------------------------------------------------------------
+#
+# Histórico:
+#
+#   v1.0 2019-10-21, Guilherme Carvalho:
+#       - Versão inicial do programa em execução sem tratamento de erros
+#
+#
+# Licença: -
+#
+#
+#######################################################################################
+#
+#                                     Menssagem
+#                                     ---------
+#
+echo '-------------------------------------------------------------------------------'
+echo '     ____               ____           __        ____   ___              __    '
+echo '    / __ \________     /  _/___  _____/ /_____ _/ / /  /   |  __________/ /_   '
+echo '   / /_/ / ___/ _ \    / // __ \/ ___/ __/ __ `/ / /  / /| | / ___/ ___/ __ \  '
+echo '  / ____/ /  /  __/  _/ // / / (__  ) /_/ /_/ / / /  / ___ |/ /  / /__/ / / /  '
+echo ' /_/   /_/   \___/  /___/_/ /_/____/\__/\__,_/_/_/  /_/  |_/_/   \___/_/ /_/   '
+echo '                                                                               '
+echo '                 Por Guilherme Carvalho - https://github.com/guilhermercarvalho'
+echo '-------------------------------------------------------------------------------'
+#
+#
+#######################################################################################
+#
+#                                     Configuração
+#                                     ------------
+#
+# Configuração das variáveis de ambiente
+#
+#       $IDIOMA_TECLADO     Keymap utilizado, padrão "br-abnt2").
+#       $TIMEZONE           Configura fuso, padrão "Sao_Paulo".
+#       $NOME_HOST          Nome da máquina.
+# 
+# --------------------------------------------------------------------------------------
+#   
+#                                  Tipo de Boot
+#                                  ---- -- ----
+#       $BOOT_UFEI          Suporte para boot do tipo UEFI
+#       $BOOT_LEGACY        Suporte para boot do tipo LEGACY
+#
+# -------------------------------------------------------------------------------------
+#
+IDIOMA_TECLADO=$(find /usr/share/kbd/keymaps/**/* -iname br-abnt2.map.gz | head -n 1)
+TIMEZONE=$(find /usr/share/zoneinfo/**/* -iname Sao_Paulo | head -n 1)
+NOME_HOST="arch"
+BOOT_UFEI=0
+BOOT_LEGACY=0
+PRE_CONFIGURADO=1
+
+#######################################################################################
+#
+#                                Messagem de Boas-Vindas
+#                                -------- -- -----------
+#
+echo "Bem-Vindo a Pré-Configuração Inicial de Instalação do Arch Linux"
+echo
+echo "Este programa pressupõe que você tenha baixado, bootado e inciado a ISO do sistema."
+read -p "Correto?[S/n] " resposta
+if [[ $resposta =~ ^[sS]([iI][mM])*$ ]]; then
+    resposta=
+    echo
+    echo "Perfeito! Deseja definir explicitamente os parâmetos de instalação?[S/n] "
+    read resposta
+    if [[ $resposta =~ ^[sS]([iI][mM])*$ ]]; then
+        resposta=
+        echo
+        echo "OK!"
+        PRE_CONFIGURADO=0
+    fi
+else
+    echo
+    echo "Execute install_iso_arch.sh"
+    echo
+
+#######################################################################################
+# 
+#                               Iniciando Instalação
+#                               --------- ----------
+#
+# Define layout do teclado
+echo "Definindo Teclado para padrão ABNT2"
+loadkeys $IDIOMA_TECLADO
+
+# Define tipo de boot disponível (Legacy ou UEFI)
+if [ -d ls /sys/firmware/efi/efivars ]; then
+    echo "Sua máquina está configurada com o sistema UEFI"
+    BOOT_UFEI=1
+else
+    echo "Sua máquina está configurada com o sistema Legacy"
+    BOOT_LEGACY=1
+fi
+
+# Testa conexão com rede
+echo "Testando conexão internet..."
+ping -c 4 -q archlinux.org
+if [ $? -eq 0 ]; then
+    echo "Conexão ativa!"
+else
+    echo "Conexão inativa!"
+    echo
+    echo "Vamos tentar realizar uma conexão utilizando o camando dhcpcd? Sim!"
+    echo
+    ip link show
+    echo
+    echo "Selecione o número de sua interface Ethernet:"
+    read num_interface
+    interface_rede=$(ip link show | grep ""$num_interface":" | cut -d\: -f2 | tr -d " ")
+    echo "Interface selecionada: "$interface_rede""
+    dhcpcd "$interface_rede"
+    ping -c 4 -q archlinux.org
+    test $? -eq 0 && echo "Conexão ativa!" || echo "Conexão ainda inativa!"
+fi
+
+# Atualiza relógio do sistema
+echo "Atualizando relógio..."
+timedatectl set-ntp true
+
+# Paticiona disco
+
+
+# Define fuso horário
+echo "Setar TIMEZONE"
+ln -sf "$TIMEZONE" /etc/localtime # São Paulo por padrão
+hwclock --systohc
+
+# Define idioma do sistema e do teclado
+echo "Definir localização e idioma"
+sed -i 's:#pt_BR.UTF-8 UTF-8:pt_BR.UTF-8 UTF-8 ; s:#pt_BR ISO-8859-1:pt_BR ISO-8859-1': /etc/locale.gen
+locale-gen
+echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+echo "KEYMAP="$IDIOMA_TECLADO"" >> /etc/vconsole.conf
+
+# Configura etc/hostname
+echo "Configurar Internet"
+echo "$NOME_HOST" >> /etc/hostname
+echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t"$NOME_HOST".localdomain "$NOME_HOST"" >> /etc/hosts
+
+# Atualiza Sistema
+echo "Update Pacman System"
+pacman -Syyuu
+
+# Define senha de root
+echo "Definir senha de root"
+passwd
+
+# Configura gerenciador de boot do sistema
+echo "Configure o boot da máquina"
+exit 0
